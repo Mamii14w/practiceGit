af546df524e46ad9df9dba0d8de711ecee3bc52e
unknown <HP@DESKTOP-0VB9K37.mylab.local>
2023-06-17 13:14:19 +0400

    i modified dagit file

--- dagit.java
+++ dagit.java
@@ -9,7 +9,7 @@ public class Question5 {
         int f1=0,f2=1,n,next;
         
         Scanner in=new Scanner(System.in);
-        
+
         System.out.println("Enter the number to be calculated");
         n=in.nextInt();
         if(n<0)
@@ -27,3 +27,19 @@ public class Question5 {
     }
     
 }
+
+
+     //create a new repository on the command line
+//echo "# practiceGit" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/Mamii14w/practiceGit.git
+//git push -u origin main
+
+
+     //push an existing repository from the command line
+//git remote add origin https://github.com/Mamii14w/practiceGit.git
+//git branch -M main
+//git push -u origin main
